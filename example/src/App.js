e9dfd3e2dd26d32c5072bb879b2dad0f58ac782d
headline-design <85418370+headline-design@users.noreply.github.com>
2021-07-29 12:08:23 -0500

    Updated App.js

--- example/src/App.js
+++ example/src/App.js
@@ -1,27 +1,71 @@
 import react, { Component } from "react";
 import Pipeline from '@pipeline-ui-2/pipeline';
 
-const address ="LMKFQIPL3VQCZGGFK4WZ7FPCQWLNBTJQ3UWSTA7D7QZSPJTZQKTDVT7WG4"
-function myBalance(){Pipeline.balance(address).then(data => alert(data));}
+//change recipient address before sending transaction
 
-/* 
- * uncomment the lines below one at a time to see how things work!
- * to send a real transaction, simply replace all arguments in Pipeline.send (except for myAlgoWallet) with the values you would like to use.
- * IMPORTANT: you transaction will be executed immediately when the app loads.
- * Therefore, it is recommended to wrap Pipeline.send in a function that is called when a button is click
-*/
+const recipient = "LMKFQIPL3VQCZGGFK4WZ7FPCQWLNBTJQ3UWSTA7D7QZSPJTZQKTDVT7WG4";
 
-// const myAlgoWallet = Pipeline.init();
-// Pipeline.connect(myAlgoWallet).then(data => {console.log(data);});
-// Pipeline.send(address, amount, note, sendingAddress, myAlgowallet, index).then(data => {console.log(data);});
+const myAlgoWallet = Pipeline.init();
+
+//amount in micro Algos
+const amount = 1;
+
+const note = "",
+
+//0 = Algorand, otherwise index number of asset
+const index = 0;
 
 class App extends Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      txID: "",
+      myAddress: "",
+      balance: 0
+    }
+  }
+
+  fetchBalance(context, returnTo) {
+    Pipeline.balance(this.state.myAddress).then(
+      data => {
+        const object = {};
+        object[returnTo] = data;
+        context.setState(object);
+      }
+    );
+  }
+  
+  handleConnect(context, returnTo) {
+    Pipeline.connect(myAlgoWallet).then(
+      data => {
+        const object = {};
+        object[returnTo] = data;
+        context.setState(object);
+      }
+    );
+  }
+  
+  handleSend(context, returnTo) {
+    Pipeline.send(recipient, amount, note, address, myAlgowallet, index).then(
+      data => {
+        const object = {};
+        object[returnTo] = data;
+        context.setState(object);
+      }
+    );
+  }
+
   render() {
     return (
-    <div align="center">
-      <h1>uncomment and edit the code to make things happen!</h1>
-      <button onClick={myBalance}>Click to check balance</button>
+    <div>
+      <h1>Edit the code to make things happen!</h1>
+      <button onClick={handleConnect(this, "myAddress")}>Click to check balance</button><br></br>
+      <button onClick={fetchBalance(this, "balance")}>Click to check balance</button><br></br>
+      <button onClick={handleSend(this, "txID")}>Click to check balance</button><br></br>
+      <p>{"Connected Address: " + this.state.address}</p><br></br>
+      <p>{"Balance: " + this.state.balance + "Micro Algos"}</p><br></br>
+      <p>{"Transaction ID: " + this.state.txID}</p><br></br>
       </div>
       );
   }
