f63578b205ab0d1d5ef9bf5253fbc98115edb2c8
Headline-Design <aaron@pipeline-ui.com>
2021-09-13 10:32:25 -0500

    Removed accidental inclusion of React code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import styles from './styles.module.css'
 import MyAlgo from '@randlabs/myalgo-connect'
 import WalletConnect from "@walletconnect/client";
 import QRCodeModal from "algorand-walletconnect-qrcode-modal";
@@ -297,64 +295,6 @@ export default class Pipeline {
             console.error(err)
         }
     }
-}
-
-export const AlgoSendButton = (props) => {
-
-    return (
-        <div>
-            <button
-                className={styles.AlgoSendButton}
-                onClick={() => {
-                    Pipeline.send(
-                        props.recipient,
-                        parseInt(props.amount || 1),
-                        props.note || "",
-                        Pipeline.myAddress,
-                        props.wallet,
-                        props.index || 0
-                    ).then(data => {
-                        if (typeof data !== 'undefined') {
-                            if (props.returnTo !== undefined) {
-                                const object = {}
-                                object[props.returnTo] = data
-                                props.context.setState(object)
-                            }
-                            if (typeof props.onChange === "function") {
-                                props.onChange(data)
-                            }
-                        }
-                    })
-                }}
-            >
-                Send
-            </button>
-        </div>
-    )
-}
-
-export const AlgoButton = (props) => {
-
-    return (
-        <button
-            className={styles.AlgoButton}
-            onClick={() => {
-                Pipeline.connect(props.wallet).then(accounts => {
-
-                    if (props.returnTo !== undefined) {
-                        const data = {};
-                        data[props.returnTo] = accounts;
-                        props.context.setState(data);
-                    }
-                    if (typeof props.onChange === "function") {
-                        props.onChange(accounts)
-                    }
-                })
-            }}
-        >
-            Connect
-        </button>
-    )
 }
 
 /* usage
